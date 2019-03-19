9dcbab1aa0f55210b2362d1ba5fc7e615b4283e0
Emmy Panken <erpanken@gmail.com>
2019-03-19 11:55:02 -0400

    backend routes draft

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router()
 module.exports = router
+// const {Song, Playlist, User} = require('./')
 
 router.use('/users', require('./users'))
 router.use('/songs', require('./songs'))
+
+// Song.belongTo(Playlist)
+// Playlist.hasMany(Song)
+// Playlist.belongsTo(User)
+// User.hasOne(Playlist)
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
---
+++ server/api/playlist.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const {Playlist} = require('../db/playlist')
+module.exports = router
+
+//create a playlist in our DB - when someone clicks "create playlist" - they will send us their playlist URL
+router.post('/create-playlist', async (req, res, next) => {
+  try {
+    playlistId = req.body.playlistId
+    const newPlaylist = await Playlist.Create({where: playlistId})
+    res.status(200).json(newPlaylist)
+  } catch (error) {
+    next(error)
+  }
+})
