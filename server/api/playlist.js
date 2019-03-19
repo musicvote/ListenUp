30fd1ed67f6becbb265dd95711704104113db8bf
Zachary Droge <zacharyadcoding@gmail.com>
2019-03-19 15:57:30 -0400

    Update playlist.js

--- server/api/playlist.js
+++ server/api/playlist.js
@@ -5,8 +5,8 @@ module.exports = router
 //create a playlist in our DB - when someone clicks "create playlist" - they will send us their playlist URL
 router.post('/create-playlist', async (req, res, next) => {
   try {
-    playlistId = req.body.playlistId
-    const newPlaylist = await Playlist.Create({where: playlistId})
+    playlistId = req.body
+    const newPlaylist = await Playlist.Create(playlistId)
     res.status(200).json(newPlaylist)
   } catch (error) {
     next(error)
