8b6f5db2a7292926f2680f836e3de29f1a5b0857
zacharyAD <zacharyadcoding@gmail.com>
2019-03-22 14:05:16 -0400

    commented out the heartbeat to merge with master

--- client/store/index.js
+++ client/store/index.js
@@ -1,19 +1,13 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
 import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
-import createHeartbeat from 'redux-heartbeat'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 import songs from './playlistStore'
-import checkHeartbeat from './middleware'
 
 const reducer = combineReducers({user, songs})
 const middleware = composeWithDevTools(
-  applyMiddleware(
-    thunkMiddleware,
-    checkHeartbeat,
-    createLogger({collapsed: true})
-  )
+  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
 const store = createStore(reducer, middleware)
 
